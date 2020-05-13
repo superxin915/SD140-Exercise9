fbf0c0db048686f6e629465c178a4a80fbfdecdd
Yuxin Li <yuxinli915@outlook.com>
2020-05-13 14:02:29 -0500

    Use GeoLocateControl ask for user location and display a button on the map when page load

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,30 @@
-navigator.geolocation.getCurrentPosition(
-  position => {
-    const coordinate = position.coords;
-    drawMap(coordinate.longitude, coordinate.latitude);
-  },
-  error => {
-    console.warn(`ERROR(${error.code}): ${error.message}`);
-  });
+// navigator.geolocation.getCurrentPosition(
+//   position => {
+//     const coordinate = position.coords;
+//     drawMap(coordinate.longitude, coordinate.latitude);
+//   },
+//   error => {
+//     console.warn(`ERROR(${error.code}): ${error.message}`);
+//   });
 
-function drawMap(longitude, latitude) {
-  mapboxgl.accessToken = `pk.eyJ1Ijoic3VwZXJ4aW4iLCJhIjoiY2thNWlqZHd4MDBpODNnb3owMDA2Y3dnYiJ9.-zciQf0emsOdjhIjB2uoNA`;
+// function drawMap(longitude, latitude) {
+mapboxgl.accessToken = `pk.eyJ1Ijoic3VwZXJ4aW4iLCJhIjoiY2thNWlqZHd4MDBpODNnb3owMDA2Y3dnYiJ9.-zciQf0emsOdjhIjB2uoNA`;
 
-  new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [longitude, latitude],
-    zoom: 13
-  })
-}
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v9',
+  // center: [longitude, latitude],
+  // zoom: 13
+});
+
+// const marker = new mapboxgl.Marker()
+//   .setLngLat([longitude, latitude])
+//   .addTo(map);
+const geoLocate = new mapboxgl.GeolocateControl();
+
+map.on('load', function() {
+  geoLocate.trigger();
+});
+
+map.addControl(geoLocate);
+// }
