80512f951b5fa602652479fce8108b3368918606
Yuxin Li <yuxinli915@outlook.com>
2020-05-14 11:19:01 -0500

    Seperate tasks for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,31 @@ const cleanCSS = require(`gulp-clean-css`);
 const htmlmin = require(`gulp-htmlmin`);
 const minify = require(`gulp-minify`);
 
-exports.default = cb => {
+function compressCSS(cb) {
   gulp.src(`css/*.css`)
     .pipe(cleanCSS())
     .pipe(gulp.dest(`dest/css`));
+  cb();
+}
 
+function compressHTML(cb) {
   gulp.src(`*.html`)
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest(`dest`));
+  cb();
+}
 
+function compressJS(cb) {
   gulp.src(`js/*.js`)
     .pipe(minify())
     .pipe(gulp.dest(`dest/js`));
-
   cb();
 }
+
+function compressFiles(cb) {
+  compressCSS(cb);
+  compressHTML(cb);
+  compressJS(cb);
+}
+
+exports.default = compressFiles;
